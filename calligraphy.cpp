44587533434d4fdef064a74228382280f7c71ff6
liana <52911552+Red-Pitaya@users.noreply.github.com>
2023-01-02 21:00:41 +0800

    Add files via upload
    calligraphy transform

---
+++ calligraphy.cpp
@@ -0,0 +1,204 @@
+#include <opencv2/opencv.hpp>
+#include<opencv2/core/core.hpp>  //Mat
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+
+{
+
+	Mat srcImg = imread("F:/学习/计算机/数字图像处理/calligraphy.png");
+
+	if (srcImg.empty()) {
+		printf("Open Error\n");
+		return -11;
+	}
+	imshow("src", srcImg);
+
+	/*blur(srcImg, srcImg, Size(2, 2));
+	imshow("blur", srcImg);
+	*/
+
+	cv::Mat hsv;
+	// hsv为3通道，hsv.channels==3
+	cv::cvtColor(srcImg, hsv, cv::COLOR_BGR2HSV);
+	std::cout << "hsv.channels=" << hsv.channels() << std::endl;
+	
+	Scalar lower_red_1 = cv::Scalar(0, 0, 0);
+	Scalar upper_red_1 = cv::Scalar(180, 255, 50);
+	Mat binImg;
+	cv::inRange(hsv, lower_red_1, upper_red_1, binImg);
+
+	imshow("bin", binImg);
+
+	int rows = binImg.rows;
+	int cols = binImg.cols * binImg.channels();   // 列项要乘通道数
+
+/*	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			binImg.at<uchar>(i, j) = 255 - binImg.at<uchar>(i, j);
+		}
+	}
+	imshow("binreverse", binImg);*/
+
+	/*
+	Mat kernel;
+	Mat dst;
+	Mat open_dst;
+	Mat result,skeleton;
+	while (1)
+	{
+		kernel = getStructuringElement(MORPH_CROSS, Size(7, 7));
+		erode(binImg, dst, kernel);
+		morphologyEx(dst, open_dst, MORPH_OPEN, kernel);
+		result = dst - open_dst;
+		bitwise_or(skeleton, result, skeleton);
+	}
+			
+	imshow("sketch", skeleton);
+	*/
+
+	vector<Point> deletelist1;
+	int Zhangmude[9];
+	int nl = binImg.rows;
+	int nc = binImg.cols;
+	while (true)
+	{
+		for (int j = 1; j < (nl - 1); j++)
+		{
+			uchar* data_last = binImg.ptr<uchar>(j - 1);
+			uchar* data = binImg.ptr<uchar>(j);
+			uchar* data_next = binImg.ptr<uchar>(j + 1);
+			for (int i = 1; i < (nc - 1); i++)
+			{
+				if (data[i] == 255)
+				{
+					Zhangmude[0] = 1;
+					if (data_last[i] == 255) Zhangmude[1] = 1;
+					else  Zhangmude[1] = 0;
+					if (data_last[i + 1] == 255) Zhangmude[2] = 1;
+					else  Zhangmude[2] = 0;
+					if (data[i + 1] == 255) Zhangmude[3] = 1;
+					else  Zhangmude[3] = 0;
+					if (data_next[i + 1] == 255) Zhangmude[4] = 1;
+					else  Zhangmude[4] = 0;
+					if (data_next[i] == 255) Zhangmude[5] = 1;
+					else  Zhangmude[5] = 0;
+					if (data_next[i - 1] == 255) Zhangmude[6] = 1;
+					else  Zhangmude[6] = 0;
+					if (data[i - 1] == 255) Zhangmude[7] = 1;
+					else  Zhangmude[7] = 0;
+					if (data_last[i - 1] == 255) Zhangmude[8] = 1;
+					else  Zhangmude[8] = 0;
+					int whitepointtotal = 0;
+					for (int k = 1; k < 9; k++)
+					{
+						whitepointtotal = whitepointtotal + Zhangmude[k];
+					}
+					if ((whitepointtotal >= 2) && (whitepointtotal <= 6))
+					{
+						int ap = 0;
+						if ((Zhangmude[1] == 0) && (Zhangmude[2] == 1)) ap++;
+						if ((Zhangmude[2] == 0) && (Zhangmude[3] == 1)) ap++;
+						if ((Zhangmude[3] == 0) && (Zhangmude[4] == 1)) ap++;
+						if ((Zhangmude[4] == 0) && (Zhangmude[5] == 1)) ap++;
+						if ((Zhangmude[5] == 0) && (Zhangmude[6] == 1)) ap++;
+						if ((Zhangmude[6] == 0) && (Zhangmude[7] == 1)) ap++;
+						if ((Zhangmude[7] == 0) && (Zhangmude[8] == 1)) ap++;
+						if ((Zhangmude[8] == 0) && (Zhangmude[1] == 1)) ap++;
+						if (ap == 1)
+						{
+							if ((Zhangmude[1] * Zhangmude[7] * Zhangmude[5] == 0) && (Zhangmude[3] * Zhangmude[5] * Zhangmude[7] == 0))
+							{
+								deletelist1.push_back(Point(i, j));
+							}
+						}
+					}
+				}
+			}
+		}
+		if (deletelist1.size() == 0) break;
+		for (size_t i = 0; i < deletelist1.size(); i++)
+		{
+			Point tem;
+			tem = deletelist1[i];
+			uchar* data = binImg.ptr<uchar>(tem.y);
+			data[tem.x] = 0;
+		}
+		deletelist1.clear();
+
+		for (int j = 1; j < (nl - 1); j++)
+		{
+			uchar* data_last = binImg.ptr<uchar>(j - 1);
+			uchar* data = binImg.ptr<uchar>(j);
+			uchar* data_next = binImg.ptr<uchar>(j + 1);
+			for (int i = 1; i < (nc - 1); i++)
+			{
+				if (data[i] == 255)
+				{
+					Zhangmude[0] = 1;
+					if (data_last[i] == 255) Zhangmude[1] = 1;
+					else  Zhangmude[1] = 0;
+					if (data_last[i + 1] == 255) Zhangmude[2] = 1;
+					else  Zhangmude[2] = 0;
+					if (data[i + 1] == 255) Zhangmude[3] = 1;
+					else  Zhangmude[3] = 0;
+					if (data_next[i + 1] == 255) Zhangmude[4] = 1;
+					else  Zhangmude[4] = 0;
+					if (data_next[i] == 255) Zhangmude[5] = 1;
+					else  Zhangmude[5] = 0;
+					if (data_next[i - 1] == 255) Zhangmude[6] = 1;
+					else  Zhangmude[6] = 0;
+					if (data[i - 1] == 255) Zhangmude[7] = 1;
+					else  Zhangmude[7] = 0;
+					if (data_last[i - 1] == 255) Zhangmude[8] = 1;
+					else  Zhangmude[8] = 0;
+					int whitepointtotal = 0;
+					for (int k = 1; k < 9; k++)
+					{
+						whitepointtotal = whitepointtotal + Zhangmude[k];
+					}
+					if ((whitepointtotal >= 2) && (whitepointtotal <= 6))
+					{
+						int ap = 0;
+						if ((Zhangmude[1] == 0) && (Zhangmude[2] == 1)) ap++;
+						if ((Zhangmude[2] == 0) && (Zhangmude[3] == 1)) ap++;
+						if ((Zhangmude[3] == 0) && (Zhangmude[4] == 1)) ap++;
+						if ((Zhangmude[4] == 0) && (Zhangmude[5] == 1)) ap++;
+						if ((Zhangmude[5] == 0) && (Zhangmude[6] == 1)) ap++;
+						if ((Zhangmude[6] == 0) && (Zhangmude[7] == 1)) ap++;
+						if ((Zhangmude[7] == 0) && (Zhangmude[8] == 1)) ap++;
+						if ((Zhangmude[8] == 0) && (Zhangmude[1] == 1)) ap++;
+						if (ap == 1)
+						{
+							if ((Zhangmude[1] * Zhangmude[3] * Zhangmude[5] == 0) && (Zhangmude[3] * Zhangmude[1] * Zhangmude[7] == 0))
+							{
+								deletelist1.push_back(Point(i, j));
+							}
+						}
+					}
+				}
+			}
+		}
+		if (deletelist1.size() == 0) break;
+		for (size_t i = 0; i < deletelist1.size(); i++)
+		{
+			Point tem;
+			tem = deletelist1[i];
+			uchar* data = binImg.ptr<uchar>(tem.y);
+			data[tem.x] = 0;
+		}
+		deletelist1.clear();
+	}
+	
+	imshow("sketch", binImg);
+
+	waitKey(0);
+
+	return 0;
+
+}
